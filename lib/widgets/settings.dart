3d851e85bd90e0e3a1cc5ce302292409be163779
magicsk <minemagicsk@gmail.com>
2019-08-06 18:46:05 +0200

    Added webview theme change

--- lib/widgets/settings.dart
+++ lib/widgets/settings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:persist_theme/persist_theme.dart';
+import 'package:preferences/preferences.dart';
 
 import 'package:mhd_virtual_table/locale/locales.dart';
 
@@ -12,12 +13,14 @@ class Settings extends StatefulWidget {
 class _SettingsState extends State<Settings> {
   bool darkTheme = false;
   bool blackTheme = false;
+  var tableTheme = 'Dark';
 
   _getprefs() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       darkTheme = prefs.getBool('darkTheme');
       blackTheme = prefs.getBool('blackTheme');
+      tableTheme = prefs.getString('tableTheme');
     });
   }
 
@@ -37,60 +40,30 @@ class _SettingsState extends State<Settings> {
         children: <Widget>[
           DarkModeSwitch(),
           TrueBlackSwitch(),
-          // SwitchListTile(
-          //   value: darkTheme,
-          //   title: Text(AppLocalizations.of(context).darkTheme),
-          //   onChanged: (value) async {
-          //     print(value);
-          //     print(darkTheme);
-          //     print(blackTheme);
-          //     setState(() {
-          //       DynamicTheme.of(context)
-          //           .setBrightness(value ? Brightness.dark : Brightness.light);
-          //       DynamicTheme.of(context).setThemeData(ThemeData(
-          //         backgroundColor: value ? null : Colors.white,
-          //         dialogBackgroundColor: value ? null : Colors.white,
-          //         scaffoldBackgroundColor: value ? null : Colors.white,
-          //         primaryColor: Colors.redAccent[700],
-          //         accentColor: Colors.red,
-          //         primaryColorDark: Colors.redAccent[700],
-          //         toggleableActiveColor: Colors.red,
-          //         primarySwatch: Colors.red,
-          //         brightness: value ? Brightness.dark : Brightness.light,
-          //       ));
-          //       darkTheme = value;
-          //       blackTheme = false;
-          //     });
-          //     SharedPreferences prefs = await SharedPreferences.getInstance();
-          //     await prefs.setBool('darkTheme', value);
-          //     await prefs.setBool('blackTheme', false);
-          //   },
-          // ),
-          // Divider(),
-          // SwitchListTile(
-          //   value: blackTheme,
-          //   title: Text(AppLocalizations.of(context).blackTheme),
-          //   onChanged: (value) async {
-          //     setState(() {
-          //       darkTheme = true;
-          //       blackTheme = value;
-          //     });
-          //     DynamicTheme.of(context).setThemeData(ThemeData(
-          //       backgroundColor: value ? Colors.black : null,
-          //       dialogBackgroundColor: value ? Colors.black : null,
-          //       scaffoldBackgroundColor: value ? Colors.black : null,
-          //       primaryColor: Colors.redAccent[700],
-          //       accentColor: Colors.red,
-          //       primaryColorDark: Colors.redAccent[700],
-          //       toggleableActiveColor: Colors.red,
-          //       primarySwatch: Colors.red,
-          //       brightness: Brightness.dark,
-          //     ));
-          //     SharedPreferences prefs = await SharedPreferences.getInstance();
-          //     await prefs.setBool('darkTheme', true);
-          //     await prefs.setBool('blackTheme', value);
-          //   },
-          // ),
+          DropdownPreference(
+            'Table theme',
+            'tableTheme',
+            defaultVal: 'Light',
+            values: ['Light', 'Dark', 'Blue'],
+            onChange: (value) async{
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              setState(() {
+                prefs.setString('tableTheme', value);
+                switch (value){
+                  case 'Dark':
+                    prefs.setInt('tableThemeInt', 0);
+                    break;
+                  case 'Light':
+                    prefs.setInt('tableThemeInt', 1);
+                    break;
+                  case 'Blue':
+                    prefs.setInt('tableThemeInt', 2);
+                    break;
+                }
+                print(prefs.getInt('tableThemeInt'));
+              });
+            },
+          ),
         ],
       ),
     );
